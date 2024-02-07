335cb7d7330e660521602007b27f6fedd5445828
khihdh <keven.bihan@free.fr>
2024-02-07 02:59:46 +0100

    get product, delete product and patch product of frontend now changesthe json backend file too

--- front/src/app/products/products.service.ts
+++ front/src/app/products/products.service.ts
@@ -1,12 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Product } from './product.class';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+
+    private apiUrl = 'http://localhost:8080';
 
     private static productslist: Product[] = null;
     private products$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
@@ -16,8 +19,8 @@ export class ProductsService {
     getProducts(): Observable<Product[]> {
         if( ! ProductsService.productslist )
         {
-            this.http.get<any>('assets/products.json').subscribe(data => {
-                ProductsService.productslist = data.data;
+            this.http.get<any>(`${this.apiUrl}/products`).subscribe(data => {
+                ProductsService.productslist = data;
                 
                 this.products$.next(ProductsService.productslist);
             });
@@ -32,13 +35,15 @@ export class ProductsService {
 
     create(prod: Product): Observable<Product[]> {
 
+        this.http.post<any>(`${this.apiUrl}/products`, prod).subscribe(data => {} );
         ProductsService.productslist.push(prod);
         this.products$.next(ProductsService.productslist);
-        
         return this.products$;
     }
 
     update(prod: Product): Observable<Product[]>{
+        const { id, ...productWithoutId } = prod;
+        this.http.patch<any>(`${this.apiUrl}/products/${prod.id}`, productWithoutId).subscribe(data => {} );
         ProductsService.productslist.forEach(element => {
             if(element.id == prod.id)
             {
@@ -56,10 +61,12 @@ export class ProductsService {
         this.products$.next(ProductsService.productslist);
 
         return this.products$;
+        return this.http.patch<Product[]>(`${this.apiUrl}/products/${prod.id}`, prod);
     }
 
 
     delete(id: number): Observable<Product[]>{
+        this.http.delete(`${this.apiUrl}/products/${id}` ).subscribe(data => {} );
         ProductsService.productslist = ProductsService.productslist.filter(value => { return value.id !== id } );
         this.products$.next(ProductsService.productslist);
         return this.products$;
